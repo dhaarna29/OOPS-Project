f196b3897d82f7dc233657a938c127bda114ff90
Meghna <31079118+noobie98@users.noreply.github.com>
2017-10-21 19:13:07 +0530

    Update shopping.cpp

--- shopping.cpp
+++ shopping.cpp
@@ -13,27 +13,45 @@ shopping::~shopping()
     //dtor
 }
 
-void shopping::display()
+int main()
 {
-    cout<<"PHONE 1 (Matte Black)\n";
-    cout<<"Rs. 2999\nYou save 10%\n";
-    cout<<"8MP primary camera and 5MP front facing camera\n";
-    cout<<"2GB RAM, 16GB internal memory expandable up to 32GB and dual SIM (micro+micro)\n";
+    int i;
+    shopping phone[4];
+    phone[0].name = "PHONE 1";
+    phone[0].colour = "Matte Black";
+    phone[0].cost = "Rs. 2999";
+    phone[0].camera = "8MP primary camera and 5MP front facing camera";
+    phone[0].memory = "2GB RAM, 16GB internal memory expandable up to 32GB and dual SIM (micro+micro)";
 
-    cout<<"PHONE 2 (Glossy Grey)\n";
-    cout<<"Rs. 4999\nYou save 17%\n";
-    cout<<"10MP primary camera and 8MP front facing camera\n";
-    cout<<"3GB RAM, 32GB internal memory with micro SD card\n";
+    phone[1].name = "PHONE 2";
+    phone[1].colour = "Glossy Grey";
+    phone[1].cost = "Rs. 4999";
+    phone[1].camera = "10MP primary camera and 8MP front facing camera";
+    phone[1].memory = "3GB RAM, 32GB internal memory with micro SD card";
 
-    cout<<"PHONE 3 (Cream Pie)\n";
-    cout<<"Rs. 7999\n";
-    cout<<"14MP primary camera and 6MP front facing camera\n";
-    cout<<"4GB RAM, 64GB internal memory expandable up to 128GB with micro SD card\n";
+    phone[2].name = "PHONE 3";
+    phone[2].colour = "Cream Pie";
+    phone[2].cost = "Rs. 7999";
+    phone[2].camera = "14MP primary camera and 6MP front facing camera";
+    phone[2].memory = "4GB RAM, 64GB internal memory expandable up to 128GB with micro SD card";
 
-    cout<<"PHONE 4 (Gold)\n";
-    cout<<"Rs. 9999\nYou save 22%\n";
-    cout<<"10MP primary camera and 8MP front facing camera\n";
-    cout<<"3GB RAM, 32GB internal memory with Hybrid SIM slot: Supports nano SIM / Micro SIM / MicroSD card\n";
+
+    phone[3].name = "PHONE 4";
+    phone[3].colour = "Gold";
+    phone[3].cost = "Rs. 9999";
+    phone[3].camera = "10MP primary camera and 8MP front facing camera";
+    phone[3].memory = "3GB RAM, 32GB internal memory with Hybrid SIM slot: Supports nano SIM / Micro SIM / MicroSD card";
+
+    cout<<"Phones available right now are\n\n";
+
+    for(int i = 0; i < 4; i++)
+    {
+        cout<<phone[i].name<<"\n";
+        cout<<phone[i].colour<<"\n";
+        cout<<phone[i].cost<<"\n";
+        cout<<phone[i].camera<<"\n";
+        cout<<phone[i].memory<<"\n\n";
+    }
+
+    return 0;
 }
-
-
