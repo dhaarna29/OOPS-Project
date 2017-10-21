80e511e134e288f66ca454c2d9219f0315888c30
Meghna <31079118+noobie98@users.noreply.github.com>
2017-10-21 19:13:57 +0530

    Update shopping.h

--- shopping.h
+++ shopping.h
@@ -1,13 +1,17 @@
 #ifndef SHOPPING_H
 #define SHOPPING_H
-
+#include<iostream>
+using namespace std;
 
 class shopping
 {
     public:
         shopping();
         ~shopping();
-        void display();
+        //void display();
+
+        string name, colour, cost, camera, memory;
+
 };
 
 #endif // SHOPPING_H
